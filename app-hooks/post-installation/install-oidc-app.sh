b7b83880c0d0019705af4406685b460e948c7738
Chris Coutinho <chris@coutinho.io>
2025-10-13 18:07:54 +0200

    chore: comments

--- app-hooks/post-installation/install-oidc-app.sh
+++ app-hooks/post-installation/install-oidc-app.sh
@@ -5,11 +5,9 @@ set -euox pipefail
 echo "Installing and configuring OIDC apps for testing..."
 
 # Enable the OIDC Identity Provider app
-#php /var/www/html/occ app:install oidc || true
 php /var/www/html/occ app:enable oidc
 
 # Enable the user_oidc app (OIDC client for bearer token validation)
-#php /var/www/html/occ app:install user_oidc || true
 php /var/www/html/occ app:enable user_oidc
 
 patch -u /var/www/html/custom_apps/user_oidc/lib/User/Backend.php -i /docker-entrypoint-hooks.d/post-installation/0001-Fix-Bearer-token-authentication-causing-session-logo.patch
