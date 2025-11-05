205c3b013cfe9bcf8ff648ba102d3afa571442de
Chris Coutinho <chris@coutinho.io>
2025-11-05 06:57:12 +0100

    build: Update oidc submodule

--- app-hooks/post-installation/10-install-oidc-app.sh
+++ app-hooks/post-installation/10-install-oidc-app.sh
@@ -31,8 +31,9 @@ else
 fi
 
 # Configure OIDC Identity Provider with dynamic client registration enabled
-php /var/www/html/occ config:app:set oidc dynamic_client_registration --value='true'
-php /var/www/html/occ config:app:set oidc proof_key_for_code_exchange --value=true --type=boolean
+php /var/www/html/occ config:app:set oidc dynamic_client_registration --value='true' --type=boolean
+php /var/www/html/occ config:app:set oidc proof_key_for_code_exchange --value='true' --type=boolean
+php /var/www/html/occ config:app:set oidc allow_user_settings --value='true' --type=boolean
 php /var/www/html/occ config:app:set oidc default_token_type --value='jwt'
 
 echo "OIDC app installed and configured successfully"
